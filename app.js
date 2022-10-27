e75f301b9c58dc30731df4160d4a2c21f7682c16
ZhukovDematey <paul.avalosar@udlap.mx>
2022-10-27 01:43:09 -0500

    Changed static files

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
-const port = 3000
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.listen(3000, () =>{
+
+app.listen(3000, () => {
     console.log('Server on port', port)
 })
 
